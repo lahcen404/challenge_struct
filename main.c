67caa7f38eea6a0b304f52ac20608e6a34ef7477
lahcen404 <lahcen.maskour2003@gmail.com>
2024-10-25 12:02:33 +0100

    Challenge struct

--- main.c
+++ main.c
@@ -1,50 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
  //Strcuture Adresse
-struct Adresse{
+typedef struct {
 char rue[100];
 char ville[30];
 int code_postal;
-};
+}Adresse;
 
 //Strcuture Personne
-struct Personne{
+typedef struct {
 
-int nom[50];
+char nom[50];
 int age;
- struct Adresse adresse; //Structure Imbriquer
+Adresse adresse; //Structure Imbriquer
 
-};
+}Personne;
 
 
 
-void creer(struct Personne personne){
+void creer(Personne *personne){
 printf("Nom :");
-scanf("%s",&personne.nom);
+scanf("%s",&personne->nom);
 printf("Age :");
-scanf("%d",&personne.age);
+scanf("%d",&personne->age);
 printf("Rue :");
-scanf("%s",&personne.adresse.rue);
+scanf("%s",&personne->adresse.rue);
 printf("Ville  :");
-scanf("%s",&personne.adresse.ville);
+scanf("%s",&personne->adresse.ville);
 printf("Code Postal :");
-scanf("%d",&personne.adresse.code_postal);
+scanf("%d",&personne->adresse.code_postal);
 }
 
-void Afficher{
-printf("Nom : %d",personne.nom);
-printf("Age : %d",Personne.age);
-printf("Rue : %d",Personne.adresse.rue);
-printf("Ville : %d",Personne.adresse.ville);
-printf("Code Postal : %d",Personne.adresse.code_postal);
+void Afficher(Personne personne){
+
+printf("Nom : %s \n",personne.nom);
+printf("Age : %d \n",personne.age);
+printf("Rue : %s \n",personne.adresse.rue);
+printf("Ville : %s \n",personne.adresse.ville);
+printf("Code Postal : %d \n",personne.adresse.code_postal);
 
 }
+void Supprimer(Personne personne[], int indice,int *nbrper ){
+for(int i=indice;i<*nbrper-1;i++){
 
+    personne[i]=personne[i=+1]; // tayzid bwa7ed wyblassi fblast lakher
+    nbrper--;
+}
+}
 
 int main()
 {
-    creer;
-    Afficher;
+    Personne p1[50];
+    int doo;
+    int i;
+    int nbrper=0;
+
+
+
+do{
+printf("what do you want to do?:\n");
+printf("1- Ajouter \n");
+printf("2- Afficher \n");
+ printf("3- Modifier \n");
+printf("4-Supprimer \n");
+printf("5-Quitter \n");
+scanf("%d",&doo);
+
+
+switch(doo){
+
+case 1:
+    if(nbrper<50){
+        creer(&p1[nbrper]);
+        nbrper++;
+    }
+
+
+    break;
+
+case 2:
+
+    if(nbrper==0){
+        printf("Aucun \n");
+    }else{
+    for(i=0;i<nbrper;i++){
+printf("-------------Inforamtion %d ------------\n",i+1);
+      Afficher(p1[i]);
+}
+    }
+    break;
+
+case 3:
+
+    break;
+
+case 4:
+if(nbrper==0){
+        printf("Aucun \n");
+        }
+        else{
+                int indice;
+            printf("Entrer l'indice pour supprimer ");
+            scanf("%d", &indice);
+            if(indice >0 && indice > nbrper){
+                    Supprimer(Personne,&nbrper,indice);
+        print("Supprimer !!");
+        }else{
+            printf("Indice Invalide")
+        }
+        }
+    break;
+}
+
+}while (doo!=5);
 
     return 0;
+
+
 }
+
